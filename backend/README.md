00923ec432d0d7906bdeaf1ae15b85db734e2ce3
r-haque <raisahxque@gmail.com>
2022-09-23 22:22:03 -0400

    Added connectivity between model, view and controller

--- backend/README.md
+++ backend/README.md
@@ -1,23 +1,29 @@
-- To install all the dependencies run 
+-   To install all the dependencies run
+
 ```bash
 npm i
 ```
-- Create a ```.env``` file and  create a variable called PORT and MONGO_PASS , and store the following values like this
+
+-   Create a `.env` file and create a variable called PORT and MONGO_PASS , and store the following values like this
+
 ```
-  PORT = 3000
-  MONGO_PASS  = "ask me i will tell u"
+  PORT = 4000
+  MONGO_PASS  = "yikMlMV1gB8wcAZo"
 ```
 
-- To run the application, start up your terminal /command prompt ,and go to the project folder and then run the following command
+-   To run the application, start up your terminal /command prompt ,and go to the project folder and then run the following command
+
+```bash
+node index.js
+```
+
+or
+
 ```bash
 nodemon index.js
 ```
 
-- **Get sample Recruiter** : GET {/find-recuiter/$id}
-   - In the request url replace $id with thec recruiter id u want to view
-   - Sample Request: http://localhost:3000/find-recuiter/632e3f93538a67e9e21cd068
-   - Sample Ouput/schema: {"_id":"632e3f93538a67e9e21cd068","name":"Pritish","email":"pritish@mail.com","age":21,"company":"UTSC","__v":0}
-    
-
-
-
+-   **Get sample Recruiter** : GET {/recruiters/:id}
+    -   In the request url replace $id with thec recruiter id u want to view
+    -   Sample Request: http://localhost:3000/recruiters/632e3f93538a67e9e21cd068
+    -   Sample Ouput/schema: {"\_id":"632e3f93538a67e9e21cd068","name":"Pritish","email":"pritish@mail.com","age":21,"company":"UTSC","\_\_v":0}
--- backend/index.js
+++ backend/index.js
@@ -1,74 +1,87 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const { json, urlencoded } = require("body-parser")
-const express = require("express")
-const mongoose = require('mongoose')
-const Recruiter = require('./models/recruiter')
+const { json, urlencoded } = require("body-parser");
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const Recruiter = require("./models/recruiter");
 
-const app = express()
-app.use(json()) // for parsing application/json
-app.use(urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-const port = process.env.PORT || 3000;
+const app = express();
+app.use(json()); // for parsing application/json
+app.use(urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+const port = process.env.PORT || 4000;
 
-const uri = `mongodb+srv://pritishstudies:${process.env.MONGO_PASS}@recruitmecluster.ojsrxbu.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://pritishstudies:${process.env.MONGO_PASS}@recruitmecluster.ojsrxbu.mongodb.net/?retryWrites=true&w=majority`;
 
 async function connect() {
     try {
-        await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-        console.log('Connected to Database')
+        await mongoose.connect(uri, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("Connected to Database");
     } catch (error) {
-        console.error(error)
+        console.error(error);
     }
 }
 
 connect();
 
+const allowedOrigins = ["http://localhost:3000"];
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+    })
+);
 
 app.listen(port, () => console.log(`Listening on port ${port}..`));
 
-
-app.post('/add-recuiter', (req, res) => {
-
+app.post("/recruiters", (req, res) => {
     if (!req.body.name || !req.body.age || !req.body.email || !req.body.company)
-        return res.status(400).send("There are missing feilds in request body")
+        return res.status(400).send("There are missing fields in request body");
     else {
         const new_recruiter = new Recruiter({
             name: req.body.name,
             age: req.body.age,
             email: req.body.email,
-            company: req.body.company
-
+            company: req.body.company,
+        });
+        new_recruiter
+            .save()
+            .then((result) => {
+                res.send(result);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+});
+app.get("/recruiters", (req, res) => {
+    Recruiter.find()
+        .then((result) => {
+            res.send(result);
         })
-        new_recruiter.save().then(
-            (result) => { res.send(result) }
-        ).catch((err) => { console.log(err) })
-    }
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
-
-})
-app.get('/get-recuiter', (req, res) => {
-    Recruiter.find().then(
-        (result) => { res.send(result) }
-    ).catch((err) => { console.log(err) })
-})
-
-app.get('/find-recuiter/:id', (req, res) => {
-
+app.get("/recruiters/:id", (req, res) => {
     if (req.params.id) {
-        Recruiter.findById(req.params.id).then(
-            (result) => { res.send(result) }
-        ).catch((err) => { console.log(err) })
+        Recruiter.findById(req.params.id)
+            .then((result) => {
+                res.send(result);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    } else {
+        res.status(400).send("There are missing fields in request body");
     }
-
-    else {
-        res.status(400).send("There are missing feilds in request body")
-    }
-
-})
+});
 
 // const inventory_routes = require('./routes/inventory_route')
 // const image_routes = require('./routes/image_route')
-
 
 // app.use('/api/inventory', inventory_routes);
 // app.use('/api/image', image_routes);
