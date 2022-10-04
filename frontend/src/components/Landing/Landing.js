3341eccee60ead70e5b45912171348810d51df74
Raymond Weng <xiang.weng3@gmail.com>
2022-10-03 23:15:17 -0400

    Added Footer and init Contact components

--- frontend/src/components/Landing/Landing.js
+++ frontend/src/components/Landing/Landing.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import Navbar from './Navbar'
 import Header from './Header'
+import About from './About'
+import ContactUs from './ContactUs'
+import Footer from './Footer'
 function Landing() {
   return (
     <div>
+      <Navbar/>
       <Header/>
+      <About/>
+      <ContactUs/>
+      <Footer/>
     </div>
   )
 }
