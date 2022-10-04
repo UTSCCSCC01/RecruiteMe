1d59dc12265a2bc98cfd8c0e59f1437173a55969
Raymond Weng <xiang.weng3@gmail.com>
2022-10-03 23:15:17 -0400

    Added Footer and init Contact components

---
+++ frontend/src/components/Landing/ContactUs.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function ContactUs() {
+  const mainStyle = {
+    minHeight: "100vh",
+    backgroundColor: "#A9ADDD",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+  return (
+  <div style={mainStyle} id="contact">
+
+  </div>
+  );
+}
+
+export default ContactUs;
