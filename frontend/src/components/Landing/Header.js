9eeaae499efe73ff44dfd4931da721fca4e0f557
Raymond Weng <xiang.weng3@gmail.com>
2022-10-03 23:13:25 -0400

    Created Header component
    Added background image, slogan

--- frontend/src/components/Landing/Header.js
+++ frontend/src/components/Landing/Header.js
@@ -1,33 +1,46 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import logo from './LogoTransparent.png';
-export default function ButtonAppBar() {
+import React from "react";
+import background from "./background.jpg";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { Collapse, IconButton } from "@mui/material";
+import { useState, useEffect } from "react";
+function Header() {
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    setChecked(true);
+  }, []);
+  const mainStyle = {
+    backgroundImage: `url(${background})`,
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+    height: "100vh",
+    display: "flex",
+    flexWrap: "wrap",
+    textAlign: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+  };
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" style={{backgroundColor:"#91A4E8"}}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }} style={{textAlign:'center'}}>
-            <img src={logo} width={250} style={{marginTop:5}}></img>
-          </Typography>
-          <Button color="inherit">Login</Button>
-          <Button color="inherit">Sign Up</Button>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <div className="header" id="header" style={mainStyle}>
+      <Collapse
+        in={checked}
+        {...(checked ? { timeout: 1500 } : {})}
+        collapsedSize={50}
+      >
+        <h1
+          style={{ fontSize: "4.5rem", color: "whitesmoke", marginBottom: "0" }}
+        >
+          Recruit<span style={{ color: "#91A4E8" }}>Me</span>
+        </h1>
+        <h2 style={{ fontSize: "2rem", color: "whitesmoke" }}>
+          Recruitment Made Simple
+        </h2>
+        <IconButton href="#about">
+          <ExpandMoreIcon style={{ fontSize: "4rem", color: "#F26419" }} />
+        </IconButton>
+      </Collapse>
+    </div>
   );
 }
+
+export default Header;
