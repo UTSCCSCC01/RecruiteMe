d3cc4f52ecc0cc59110ad7c38f6e7fca105fd1e3
AltonLiu <alton.liu.cs@gmail.com>
2022-10-06 20:13:04 -0400

    NOT-45: Added functionality to upload and retrieve profile pictures

--- backend/models/JobSeeker.js
+++ backend/models/JobSeeker.js
@@ -1,5 +1,5 @@
 const { Binary } = require('mongodb');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const jobseekerSchema = new Schema({
