c5ed598d6f6da2d4872a2ac8b718af5cca179f64
BillyZ1435 <110247708+BillyZ1435@users.noreply.github.com>
2022-10-21 18:39:39 -0400

    NF-7: Added new post button (#58)
    * NF-7: Created display for job post and applicants
    
    * NF-7: Added temporary connection to backend
    
    Needs job post to test
    
    * NF-7: Connected to backend
    
    Uses uid to search jobseekers and photo
    
    * NF-7: Bug fix
    
    Gave each child a unique key
    
    * NF-7: Implemented Viewing other profiles
    
    * NF-7: Profile display fix
    
    * NF-7: Removed some console.log
    
    * NF-7: Commit error

--- frontend/src/components/Dashboard/JobPostTracker.js
+++ frontend/src/components/Dashboard/JobPostTracker.js
@@ -2,6 +2,9 @@ import * as React from 'react';
 import { Button, styled, ListItemButton , ListSubheader , Box , List, ListItem, ListItemAvatar, ListItemText, Avatar, Grid } from '@mui/material';
 import { ApplicantTracker } from './ApplicantTracker';
 import RecruiterController from '../../controller/RecruiterController';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import Modal from "@mui/material/Modal";
+import JobPostingForm from '../CreateJobPostingForm/JobPostingForm';
 
 const Demo = styled('div')(({ theme }) => ({
   backgroundColor: theme.palette.background.paper,
@@ -18,6 +21,13 @@ export default function JobPostTracker() {
     });
   }, []);
 
+  const handleOpenPostJobForm = () => {
+    setOpenJobPostingForm(true);
+  };
+
+  const handleCloseJobPostingForm = () => {
+    setOpenJobPostingForm(false); window.location.reload(false);
+  };
   const handleJobPostClicked = (event, index) => {
     setSelectedIndex(index);
     setCurApps(jobPosts[index].applicants);
@@ -29,14 +39,37 @@ export default function JobPostTracker() {
 
   return (
     <div>
+      <Modal
+          open={openJobPostingForm}
+          onClose={handleCloseJobPostingForm}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+      >
+          <JobPostingForm close={handleCloseJobPostingForm}></JobPostingForm>
+      </Modal>
       <Grid container >
       <Box sx={{ width: "25%", height: 700,}}>
         <Box sx={{height: 70,}}></Box>
         <Demo>
           <List sx={{ width: '100%', height: '100%',maxWidth: 350}}>
-            <ListSubheader sx={{fontSize:20}} color="inherit">My Job Posts</ListSubheader>
+          <ListSubheader sx={{fontSize:20}} color="inherit">My Job Posts
+              <Button
+                    onClick={handleOpenPostJobForm}
+                    startIcon={<AddCircleIcon fontSize="large" />}
+                    sx={{
+                        color: "black",
+                        fontSize: "20px",
+                        fontWeight: "400",
+                        textTransform: "none",
+                        marginLeft: "66px",
+                    }}
+                    size="145px"
+                >
+                New
+              </Button>
+            </ListSubheader>
             
-            {jobPosts.map(item => (
+            {jobPosts && jobPosts.map(item => (
               <ListItemButton
                 key={item._id}
                 selected={selectedIndex === jobPosts.indexOf(item)}
